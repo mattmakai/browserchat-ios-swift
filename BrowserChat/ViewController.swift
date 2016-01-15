5a005fc979f43fe51bbffa691f580490d4718869
Matt Makai <matthew.makai@gmail.com>
2016-01-15 13:59:18 -0800

    Initial Commit

---
+++ BrowserChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrowserChat
+//
+//  Created by Matthew Makai on 1/15/16.
+//  Copyright © 2016 Matthew Makai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
