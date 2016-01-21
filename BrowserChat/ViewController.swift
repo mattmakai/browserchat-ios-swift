2e54603877cba3cd9400c2017ac37f9150990d34
Matt Makai <matthew.makai@gmail.com>
2016-01-21 08:57:41 +0000

    working on stack views and layout

--- BrowserChat/ViewController.swift
+++ BrowserChat/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    let url = NSURL(string: "https://www.fullstackpython.com/")!
+    webBrowser.loadRequest(NSURLRequest(URL: url))
+
   }
 
   override func didReceiveMemoryWarning() {
